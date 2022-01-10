4d0010df25210ba408c5a7c08e5e558ee03622d1
skarab42 <skarab@bluewin.ch>
2022-01-10 16:31:09 +0100

    watch output file only

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -1,10 +1,19 @@
 import os from "os";
 import path from "path";
 
+const port = 8080;
+
+const clientPath = path.join(__dirname, "../../client/dist");
+const publicPath = path.resolve(os.homedir(), "qr-watch");
+const outputPath = path.resolve(publicPath, "qr.png");
+
+// @ts-expect-error noPropertyAccessFromIndexSignature
+const dev = process.env.NODE_ENV === "dev";
+
 export default {
-  port: 8080,
-  publicPath: path.resolve(os.homedir(), "qr-watch"),
-  clientPath: path.join(__dirname, "../../client/dist"),
-  // @ts-expect-error noPropertyAccessFromIndexSignature
-  dev: process.env.NODE_ENV === "dev",
+  dev,
+  port,
+  clientPath,
+  publicPath,
+  outputPath,
 };
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,18 @@
+import path from "path";
 import config from "./config";
 import server, { WatcherHandler, WebsocketHandler } from "./server";
 
-const watcherHandler: WatcherHandler = (err, events) => {
-  console.log("Watch:", err, events);
+const watcherHandler: WatcherHandler = (error, events) => {
+  if (error) {
+    app.server.log.error(`WatcherHandler: ${error}`);
+    return;
+  }
+
+  events.forEach((event) => {
+    if (path.resolve(event.path) === config.outputPath) {
+      console.log("QR event:", event.type);
+    }
+  });
 };
 
 const websocketHandler: WebsocketHandler = (connection) => {
