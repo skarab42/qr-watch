ec1d8362bc68605fdcf5db8dd0d79c37945539b9
skarab42 <skarab@bluewin.ch>
2022-01-10 11:45:40 +0100

    ensure public directory exists

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,11 @@
 import path from "path";
 import server, { Connection } from "./server";
 
+const outputDir = path.resolve(process.cwd(), "output");
+
 const app = server({
   port: 8080,
+  publicPath: outputDir,
   clientPath: path.join(__dirname, "../../client/dist"),
   // @ts-expect-error noPropertyAccessFromIndexSignature
   dev: process.env.NODE_ENV === "dev",
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,5 +1,6 @@
 import open from "open";
 import fsStatic from "fastify-static";
+import { ensureDirSync } from "fs-extra";
 import fastify, { FastifyInstance } from "fastify";
 import fsWebsocket, { SocketStream, WebsocketHandler } from "fastify-websocket";
 
@@ -8,6 +9,7 @@ export type Connection = SocketStream;
 interface Settings {
   dev?: boolean;
   port: number;
+  publicPath: string;
   clientPath: string;
   websocketHandler: WebsocketHandler;
 }
@@ -25,8 +27,12 @@ export default function server(settings: Settings) {
 function create(settings: Settings): FastifyInstance {
   const server = fastify({ logger: settings.dev });
 
+  ensureDirSync(settings.publicPath);
+
   server.register(fsWebsocket);
-  server.register(fsStatic, { root: settings.clientPath });
+  server.register(fsStatic, {
+    root: [settings.publicPath, settings.clientPath],
+  });
   server.get("/ws", { websocket: true }, settings.websocketHandler);
 
   return server;
