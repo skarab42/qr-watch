816f5e358c13a44606c27ab197dc2aa17f765d7d
skarab42 <skarab@bluewin.ch>
2022-01-17 15:02:42 +0100

    add disconnected message

--- packages/client/src/App.svelte
+++ packages/client/src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import WS from "./lib/ws";
+  import { name, version } from "../package.json";
   import Options from "./components/Options.svelte";
   import CreateForm from "./components/CreateForm.svelte";
   import CodePreview from "./components/CodePreview.svelte";
@@ -9,6 +10,7 @@
   let code = "";
   let file = "";
   let isValid = false;
+  let connected = false;
 
   const sounds = {
     ok: new Audio("sounds/ok.wav"),
@@ -26,10 +28,12 @@
   const ws = new WS(`ws://${location.host}/ws`);
 
   ws.on("open", () => {
+    connected = true;
     ws.emit({ type: "get-code" });
   });
 
   ws.on("close", () => {
+    connected = false;
     console.log("close");
   });
 
@@ -78,7 +82,12 @@
 </script>
 
 <main>
-  {#if code}
+  {#if !connected}
+    <div class="disconnected">
+      Server disconnected, please restart it and refresh the page. Or close the
+      page if your work is done.
+    </div>
+  {:else if code}
     <CodePreview
       {file}
       {code}
@@ -90,6 +99,7 @@
   {:else}
     <CreateForm value={code} on:update={onCreateFormUpdate} />
   {/if}
+  <div class="copyright">Powered by {name} v{version}</div>
 </main>
 
 <style>
@@ -113,5 +123,21 @@
 
   main {
     padding: 8px;
+  }
+
+  .disconnected {
+    width: 400px;
+    padding: 8px;
+    border-radius: 4px;
+    margin-bottom: 8px;
+    text-align: justify;
+    background-color: rgb(72, 75, 105);
+  }
+
+  .copyright {
+    width: 400px;
+    font-size: 12px;
+    text-align: center;
+    color: rgb(82, 85, 100);
   }
 </style>
